a6a540849fa1b587428b4cd4282743f7e150a951
GiverPlay007 <play.giver.007@gmail.com>
2021-12-03 22:41:46 -0300

    Projeto criado

---
+++ src/main/java/net/focaenterprises/peach/Launcher.java
@@ -0,0 +1,7 @@
+package net.focaenterprises.peach;
+
+public class Launcher {
+  public static void main(String[] args) {
+    new Peach().start();
+  }
+}
---
+++ src/main/java/net/focaenterprises/peach/Peach.java
@@ -0,0 +1,10 @@
+package net.focaenterprises.peach;
+
+public class Peach {
+  public Peach() {
+  }
+
+  public void start() {
+    System.out.println("Hello world!");
+  }
+}
